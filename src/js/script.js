51747e8eba38274f5df05f20bb824bf2b352d5c7
Egor <grafushka10@mail.ru>
2021-05-13 21:28:13 +0300

    vse gotovo

--- src/js/script.js
+++ src/js/script.js
@@ -46,6 +46,21 @@ toogleContent.forEach(item=>{
     });
 });
 
+$('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
+
+        $('form').trigger('reset');
+    });
+    return false;
+});
 
 $(document).ready(function(){
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
@@ -53,6 +68,62 @@ $(document).ready(function(){
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
           .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
       });
+      new WOW().init();
   });
+
+
+const btnConsultations = document.querySelectorAll('[data-modal]'),
+    btnOrders = document.querySelectorAll('.button_mini'),
+    btnBuy = document.querySelectorAll('.button__submit'),
+    overlay = document.querySelector('.overlay'),
+    modalClose = document.querySelectorAll('.modal__close'),
+    modalConsultation = document.querySelector('#consultation'),
+    modalOrder = document.querySelector('#order'),
+    modalThanks = document.querySelector('#thanks');
+
+
+btnConsultations.forEach ( (e)=>{
+    e.addEventListener('click', ()=>{
+        overlay.style.display='block';
+        modalConsultation.style.display='block';
+    });
+});
+
+btnOrders.forEach ( (e)=>{
+    e.addEventListener('click', ()=>{
+        overlay.style.display='block';
+        modalOrder.style.display='block';
+    });
+});
+
+modalClose.forEach(  (e)=> {
+    e.addEventListener('click', ()=>{
+        overlay.style.display='none';
+        modalConsultation.style.display='none';
+        modalOrder.style.display='none';
+        modalThanks.style.display='none';
+    });
+});
+
+btnBuy.forEach ( (e)=>{
+    e.addEventListener('click', ()=>{
+        overlay.style.display='block';
+        modalOrder.style.display='none';
+        modalThanks.style.display='block';
+    });
+});
+
+ // Smooth scroll and pageup
+
+ $(window).scroll(function() {
+    if ($(this).scrollTop() > 1600) {
+        $('.pageup').fadeIn();
+    } else {
+        $('.pageup').fadeOut();
+    }
+});
+
+
+
 
 
